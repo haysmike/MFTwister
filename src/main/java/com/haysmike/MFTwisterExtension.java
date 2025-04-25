c0d938e47e4c14da25d919e9784abe445328aabf
Mike Hays <haysmikethere@gmail.com>
2025-04-25 15:33:09 -0700

    Add "shifted" CC functionality

--- src/main/java/com/haysmike/MFTwisterExtension.java
+++ src/main/java/com/haysmike/MFTwisterExtension.java
@@ -3,13 +3,22 @@ package com.haysmike;
 import com.bitwig.extension.api.util.midi.ShortMidiMessage;
 import com.bitwig.extension.callback.DoubleValueChangedCallback;
 import com.bitwig.extension.callback.ShortMidiMessageReceivedCallback;
-import com.bitwig.extension.controller.api.*;
 import com.bitwig.extension.controller.ControllerExtension;
+import com.bitwig.extension.controller.api.ControllerHost;
+import com.bitwig.extension.controller.api.MidiOut;
+import com.bitwig.extension.controller.api.Parameter;
+import com.bitwig.extension.controller.api.UserControlBank;
+
+import java.util.HashSet;
+import java.util.Set;
 
 public class MFTwisterExtension extends ControllerExtension {
-    private static final int numMidiSteps = 128;
-    private static final int maxMidiValue = numMidiSteps - 1;
-    private static final int numControls = 16;
+    private static final int numCoarseMidiSteps = 128;
+    private static final int numFineMidiSteps = 256;
+    private static final int maxMidiValue = numCoarseMidiSteps - 1;
+    private static final int numControls = 64;
+
+    private static final Set<Integer> shiftedCcs = new HashSet<>();
 
     protected MFTwisterExtension(final MFTwisterExtensionDefinition definition, final ControllerHost host) {
         super(definition, host);
@@ -37,22 +46,30 @@ public class MFTwisterExtension extends ControllerExtension {
         host.getMidiInPort(0).setMidiCallback(new ShortMidiMessageReceivedCallback() {
             @Override
             public void midiReceived(ShortMidiMessage msg) {
-                host.showPopupNotification(msg.toString());
                 int cc = msg.getData1();
-                if (msg.isControlChange() && msg.getChannel() == 0 && cc < numControls) {
-                    userControlBank.getControl(cc).inc(msg.getData2() - 64, numMidiSteps);
+                int value = msg.getData2();
+                if (msg.isControlChange() && msg.getChannel() == 1 && cc < numControls) {
+                    if (value == 127) {
+                        shiftedCcs.add(cc);
+                    } else if (value == 0) {
+                        shiftedCcs.remove(cc);
+                    }
+                } else if (msg.isControlChange() && msg.getChannel() == 0 && cc < numControls && isValidIncrementalValue(value)) {
+                    int resolution = shiftedCcs.contains(cc) ? numCoarseMidiSteps : numFineMidiSteps;
+                    userControlBank.getControl(cc).inc(value - 64, resolution);
                 }
             }
         });
-
-        host.showPopupNotification("MFTwister Initialized");
     }
 
     @Override
-    public void exit() {
-        getHost().showPopupNotification("MFTwister Exited");
-    }
+    public void exit() {}
 
     @Override
-    public void flush() {}
+    public void flush() {
+    }
+
+    private boolean isValidIncrementalValue(int value) {
+        return value == 63 || value == 65;
+    }
 }
