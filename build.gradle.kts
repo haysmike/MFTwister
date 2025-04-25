9406a448b471322b99a1b7c1b4d3239a8c2a84b8
Mike Hays <haysmikethere@gmail.com>
2025-04-25 12:34:09 -0700

    Add install task

--- build.gradle.kts
+++ build.gradle.kts
@@ -13,6 +13,18 @@ dependencies {
     api(libs.bitwig.extension.api)
 }
 
+val bitwigExtensionsDir: String by project
+tasks.register<Copy>("install") {
+    from("build/libs") {
+        include("mftwister-0.1.jar")
+        rename {
+            "MFTwister.bwextension"
+        }
+    }
+    into(bitwigExtensionsDir)
+    dependsOn(tasks.jar)
+}
+
 group = "com.haysmike"
 version = "0.1"
 description = "MFTwister"
