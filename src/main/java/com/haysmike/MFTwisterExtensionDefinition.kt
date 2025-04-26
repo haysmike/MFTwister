60c8428e6a6211aebab76e3aa2979ff8dca86137
Mike Hays <haysmikethere@gmail.com>
2025-04-25 20:03:02 -0700

    Clean up Kotlin migration

--- src/main/java/com/haysmike/MFTwisterExtensionDefinition.kt
+++ src/main/java/com/haysmike/MFTwisterExtensionDefinition.kt
@@ -4,54 +4,34 @@ import com.bitwig.extension.api.PlatformType
 import com.bitwig.extension.controller.AutoDetectionMidiPortNamesList
 import com.bitwig.extension.controller.ControllerExtensionDefinition
 import com.bitwig.extension.controller.api.ControllerHost
-import java.util.*
+import kotlin.uuid.ExperimentalUuidApi
+import kotlin.uuid.Uuid
+import kotlin.uuid.toJavaUuid
+
 
 class MFTwisterExtensionDefinition : ControllerExtensionDefinition() {
-    override fun getName(): String {
-        return "MFTwister"
+    override fun getName() = "MFTwister"
+
+    override fun getAuthor() = "haysmike"
+
+    override fun getVersion() = "0.1"
+
+    @OptIn(ExperimentalUuidApi::class)
+    override fun getId() = Uuid.parse("dc337c96-9a72-47aa-9d60-b8f7766419bb").toJavaUuid()
+
+    override fun getHardwareVendor() = "DJ TechTools"
+
+    override fun getHardwareModel() = "MIDI Fighter Twister"
+
+    override fun getRequiredAPIVersion() = 21
+
+    override fun getNumMidiInPorts() = 1
+
+    override fun getNumMidiOutPorts() = 1
+
+    override fun listAutoDetectionMidiPortNames(list: AutoDetectionMidiPortNamesList, platformType: PlatformType) {
+        list.add(arrayOf("Midi Fighter Twister"), arrayOf("Midi Fighter Twister"))
     }
 
-    override fun getAuthor(): String {
-        return "haysmike"
-    }
-
-    override fun getVersion(): String {
-        return "0.1"
-    }
-
-    override fun getId(): UUID {
-        return DRIVER_ID
-    }
-
-    override fun getHardwareVendor(): String {
-        return "DJ TechTools"
-    }
-
-    override fun getHardwareModel(): String {
-        return "MIDI Fighter Twister"
-    }
-
-    override fun getRequiredAPIVersion(): Int {
-        return 21
-    }
-
-    override fun getNumMidiInPorts(): Int {
-        return 1
-    }
-
-    override fun getNumMidiOutPorts(): Int {
-        return 1
-    }
-
-    override fun listAutoDetectionMidiPortNames(list: AutoDetectionMidiPortNamesList, platformType: PlatformType?) {
-        list.add(arrayOf<String>("Midi Fighter Twister"), arrayOf<String>("Midi Fighter Twister"))
-    }
-
-    override fun createInstance(host: ControllerHost?): MFTwisterExtension {
-        return MFTwisterExtension(this, host)
-    }
-
-    companion object {
-        private val DRIVER_ID: UUID = UUID.fromString("dc337c96-9a72-47aa-9d60-b8f7766419bb")
-    }
+    override fun createInstance(host: ControllerHost) = MFTwisterExtension(this, host)
 }
