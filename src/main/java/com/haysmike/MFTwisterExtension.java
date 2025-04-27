689a990667ec2941632ee83580803fe1f3ad6813
Mike Hays <haysmikethere@gmail.com>
2025-04-26 22:57:04 -0700

    Format with google-java-format

--- src/main/java/com/haysmike/MFTwisterExtension.java
+++ src/main/java/com/haysmike/MFTwisterExtension.java
@@ -1,59 +1,73 @@
 package com.haysmike;
 
 import com.bitwig.extension.api.util.midi.ShortMidiMessage;
+import com.bitwig.extension.callback.StringValueChangedCallback;
 import com.bitwig.extension.controller.ControllerExtension;
 import com.bitwig.extension.controller.api.*;
 
 public class MFTwisterExtension extends ControllerExtension {
-    private static final int numControls = 64;
-    private static final int maxMidiCcValue = 127;
-    public static final double defaultSensitivity = 1.0;
-    public static final double shiftedSensitivity = 0.5;
+  private static final int numControls = 64;
+  private static final int maxMidiCcValue = 127;
+  public static final double defaultSensitivity = 1.0;
+  public static final double shiftedSensitivity = 0.5;
 
-    private HardwareSurface hardwareSurface;
-    private MidiOut midiOut;
+  private HardwareSurface hardwareSurface;
+  private MidiOut midiOut;
 
-    protected MFTwisterExtension(final MFTwisterExtensionDefinition definition, final ControllerHost host) {
-        super(definition, host);
+  protected MFTwisterExtension(
+      final MFTwisterExtensionDefinition definition, final ControllerHost host) {
+    super(definition, host);
+  }
+
+  @Override
+  public void init() {
+    final ControllerHost host = getHost();
+
+    MidiIn midiIn = host.getMidiInPort(0);
+    midiOut = host.getMidiOutPort(0);
+
+    hardwareSurface = host.createHardwareSurface();
+
+    for (int i = 0; i < numControls; i++) {
+      final int cc = i;
+      final int encoderNumber = i + 1;
+      RelativeHardwareKnob relativeHardwareKnob =
+          hardwareSurface.createRelativeHardwareKnob(String.format("Enc %02d", encoderNumber));
+      relativeHardwareKnob.setAdjustValueMatcher(
+          midiIn.createRelativeBinOffsetCCValueMatcher(0, cc, maxMidiCcValue));
+      relativeHardwareKnob
+          .modulatedTargetValue()
+          .addValueObserver(value -> sendMidiCc(0, cc, (int) (value * maxMidiCcValue)));
+      relativeHardwareKnob
+          .hasTargetValue()
+          .addValueObserver(value -> sendMidiCc(2, cc, value ? 47 : 17));
+
+      HardwareButton hardwareButton =
+          hardwareSurface.createHardwareButton(String.format("Switch %02d", encoderNumber));
+      HardwareAction pressedAction = hardwareButton.pressedAction();
+      pressedAction.setActionMatcher(midiIn.createCCActionMatcher(1, cc, maxMidiCcValue));
+      pressedAction.setBinding(
+          host.createCallbackAction(
+              () -> relativeHardwareKnob.setSensitivity(shiftedSensitivity),
+              () -> "Handle switch pressed"));
+      HardwareAction releasedAction = hardwareButton.releasedAction();
+      releasedAction.setActionMatcher(midiIn.createCCActionMatcher(1, cc, 0));
+      releasedAction.setBinding(
+          host.createCallbackAction(
+              () -> relativeHardwareKnob.setSensitivity(defaultSensitivity),
+              () -> "Handle switch released"));
     }
+  }
 
-    @Override
-    public void init() {
-        final ControllerHost host = getHost();
+  @Override
+  public void exit() {}
 
-        MidiIn midiIn = host.getMidiInPort(0);
-        midiOut = host.getMidiOutPort(0);
+  @Override
+  public void flush() {
+    hardwareSurface.updateHardware();
+  }
 
-        hardwareSurface = host.createHardwareSurface();
-
-        for (int i = 0; i < numControls; i++) {
-            final int cc = i;
-            final int encoderNumber = i + 1;
-            RelativeHardwareKnob relativeHardwareKnob = hardwareSurface.createRelativeHardwareKnob("Encoder " + encoderNumber);
-            relativeHardwareKnob.setAdjustValueMatcher(midiIn.createRelativeBinOffsetCCValueMatcher(0, cc, maxMidiCcValue));
-            relativeHardwareKnob.modulatedTargetValue().addValueObserver(value -> sendMidiCc(0, cc, (int) (value * maxMidiCcValue)));
-            relativeHardwareKnob.hasTargetValue().addValueObserver(value -> sendMidiCc(2, cc, value ? 47 : 17));
-
-            HardwareButton hardwareButton = hardwareSurface.createHardwareButton("Switch " + encoderNumber);
-            HardwareAction pressedAction = hardwareButton.pressedAction();
-            pressedAction.setActionMatcher(midiIn.createCCActionMatcher(1, cc, maxMidiCcValue));
-            pressedAction.setBinding(host.createCallbackAction(() -> relativeHardwareKnob.setSensitivity(shiftedSensitivity), () -> "Handle switch pressed"));
-            HardwareAction releasedAction = hardwareButton.releasedAction();
-            releasedAction.setActionMatcher(midiIn.createCCActionMatcher(1, cc, 0));
-            releasedAction.setBinding(host.createCallbackAction(() -> relativeHardwareKnob.setSensitivity(defaultSensitivity), () -> "Handle switch released"));
-        }
-    }
-
-    @Override
-    public void exit() {
-    }
-
-    @Override
-    public void flush() {
-        hardwareSurface.updateHardware();
-    }
-
-    private void sendMidiCc(int channel, int cc, int value) {
-        midiOut.sendMidi(ShortMidiMessage.CONTROL_CHANGE | channel, cc, value);
-    }
+  private void sendMidiCc(int channel, int cc, int value) {
+    midiOut.sendMidi(ShortMidiMessage.CONTROL_CHANGE | channel, cc, value);
+  }
 }
--- src/main/java/com/haysmike/MFTwisterExtensionDefinition.java
+++ src/main/java/com/haysmike/MFTwisterExtensionDefinition.java
@@ -8,60 +8,61 @@ import com.bitwig.extension.controller.api.ControllerHost;
 import java.util.UUID;
 
 public class MFTwisterExtensionDefinition extends ControllerExtensionDefinition {
-    private static final UUID DRIVER_ID = UUID.fromString("dc337c96-9a72-47aa-9d60-b8f7766419bb");
+  private static final UUID DRIVER_ID = UUID.fromString("dc337c96-9a72-47aa-9d60-b8f7766419bb");
 
-    @Override
-    public String getName() {
-        return "MFTwister";
-    }
+  @Override
+  public String getName() {
+    return "MFTwister";
+  }
 
-    @Override
-    public String getAuthor() {
-        return "haysmike";
-    }
+  @Override
+  public String getAuthor() {
+    return "haysmike";
+  }
 
-    @Override
-    public String getVersion() {
-        return "0.1";
-    }
+  @Override
+  public String getVersion() {
+    return "0.1";
+  }
 
-    @Override
-    public UUID getId() {
-        return DRIVER_ID;
-    }
+  @Override
+  public UUID getId() {
+    return DRIVER_ID;
+  }
 
-    @Override
-    public String getHardwareVendor() {
-        return "DJ TechTools";
-    }
+  @Override
+  public String getHardwareVendor() {
+    return "DJ TechTools";
+  }
 
-    @Override
-    public String getHardwareModel() {
-        return "MIDI Fighter Twister";
-    }
+  @Override
+  public String getHardwareModel() {
+    return "MIDI Fighter Twister";
+  }
 
-    @Override
-    public int getRequiredAPIVersion() {
-        return 21;
-    }
+  @Override
+  public int getRequiredAPIVersion() {
+    return 21;
+  }
 
-    @Override
-    public int getNumMidiInPorts() {
-        return 1;
-    }
+  @Override
+  public int getNumMidiInPorts() {
+    return 1;
+  }
 
-    @Override
-    public int getNumMidiOutPorts() {
-        return 1;
-    }
+  @Override
+  public int getNumMidiOutPorts() {
+    return 1;
+  }
 
-    @Override
-    public void listAutoDetectionMidiPortNames(final AutoDetectionMidiPortNamesList list, final PlatformType platformType) {
-        list.add(new String[]{"Midi Fighter Twister"}, new String[]{"Midi Fighter Twister"});
-    }
+  @Override
+  public void listAutoDetectionMidiPortNames(
+      final AutoDetectionMidiPortNamesList list, final PlatformType platformType) {
+    list.add(new String[] {"Midi Fighter Twister"}, new String[] {"Midi Fighter Twister"});
+  }
 
-    @Override
-    public MFTwisterExtension createInstance(final ControllerHost host) {
-        return new MFTwisterExtension(this, host);
-    }
+  @Override
+  public MFTwisterExtension createInstance(final ControllerHost host) {
+    return new MFTwisterExtension(this, host);
+  }
 }
