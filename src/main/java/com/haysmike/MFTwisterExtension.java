e1c2b872c5f95b634fa17abb32bcac6f1c789275
Mike Hays <haysmikethere@gmail.com>
2025-04-27 01:38:01 -0700

    Convert magic numbers to constants

--- src/main/java/com/haysmike/MFTwisterExtension.java
+++ src/main/java/com/haysmike/MFTwisterExtension.java
@@ -1,15 +1,26 @@
 package com.haysmike;
 
 import com.bitwig.extension.api.util.midi.ShortMidiMessage;
-import com.bitwig.extension.callback.StringValueChangedCallback;
 import com.bitwig.extension.controller.ControllerExtension;
 import com.bitwig.extension.controller.api.*;
 
 public class MFTwisterExtension extends ControllerExtension {
-  private static final int numControls = 64;
-  private static final int maxMidiCcValue = 127;
-  public static final double defaultSensitivity = 1.0;
-  public static final double shiftedSensitivity = 0.5;
+  private static final int NUM_ENCODERS = 64;
+
+  private static final int ENCODER_INPUT_CHANNEL = 0;
+  private static final int INDICATOR_RING_OUTPUT_CHANNEL = 0;
+  private static final int MAX_CC_VALUE = 127;
+
+  private static final int SWITCH_INPUT_CHANNEL = 1;
+  private static final int SWITCH_OFF_VALUE = 0;
+  private static final int SWITCH_ON_VALUE = 127;
+
+  private static final int RGB_ANIMATION_OUTPUT_CHANNEL = 2;
+  private static final int RGB_OFF_VALUE = 17;
+  private static final int RGB_MAX_BRIGHTNESS_VALUE = 47;
+
+  public static final double DEFAULT_ENCODER_SENSITIVITY = 1.0;
+  public static final double SHIFTED_ENCODER_SENSITIVITY = 0.5;
 
   private HardwareSurface hardwareSurface;
   private MidiOut midiOut;
@@ -28,39 +39,49 @@ public class MFTwisterExtension extends ControllerExtension {
 
     hardwareSurface = host.createHardwareSurface();
 
-    for (int i = 0; i < numControls; i++) {
+    for (int i = 0; i < NUM_ENCODERS; i++) {
       final int cc = i;
       final int encoderNumber = i + 1;
       RelativeHardwareKnob relativeHardwareKnob =
           hardwareSurface.createRelativeHardwareKnob(String.format("Enc %02d", encoderNumber));
       relativeHardwareKnob.setAdjustValueMatcher(
-          midiIn.createRelativeBinOffsetCCValueMatcher(0, cc, maxMidiCcValue));
+          midiIn.createRelativeBinOffsetCCValueMatcher(ENCODER_INPUT_CHANNEL, cc, MAX_CC_VALUE));
       relativeHardwareKnob
           .modulatedTargetValue()
-          .addValueObserver(value -> sendMidiCc(0, cc, (int) (value * maxMidiCcValue)));
+          .addValueObserver(
+              value -> sendMidiCc(INDICATOR_RING_OUTPUT_CHANNEL, cc, (int) (value * MAX_CC_VALUE)));
       relativeHardwareKnob
           .hasTargetValue()
-          .addValueObserver(value -> sendMidiCc(2, cc, value ? 47 : 17));
+          .addValueObserver(
+              value ->
+                  sendMidiCc(
+                          RGB_ANIMATION_OUTPUT_CHANNEL, cc, value ? RGB_MAX_BRIGHTNESS_VALUE : RGB_OFF_VALUE));
 
       HardwareButton hardwareButton =
           hardwareSurface.createHardwareButton(String.format("Switch %02d", encoderNumber));
       HardwareAction pressedAction = hardwareButton.pressedAction();
-      pressedAction.setActionMatcher(midiIn.createCCActionMatcher(1, cc, maxMidiCcValue));
+      pressedAction.setActionMatcher(
+          midiIn.createCCActionMatcher(SWITCH_INPUT_CHANNEL, cc, SWITCH_ON_VALUE));
       pressedAction.setBinding(
           host.createCallbackAction(
-              () -> relativeHardwareKnob.setSensitivity(shiftedSensitivity),
+              () -> relativeHardwareKnob.setSensitivity(SHIFTED_ENCODER_SENSITIVITY),
               () -> "Handle switch pressed"));
       HardwareAction releasedAction = hardwareButton.releasedAction();
-      releasedAction.setActionMatcher(midiIn.createCCActionMatcher(1, cc, 0));
+      releasedAction.setActionMatcher(
+          midiIn.createCCActionMatcher(SWITCH_INPUT_CHANNEL, cc, SWITCH_OFF_VALUE));
       releasedAction.setBinding(
           host.createCallbackAction(
-              () -> relativeHardwareKnob.setSensitivity(defaultSensitivity),
+              () -> relativeHardwareKnob.setSensitivity(DEFAULT_ENCODER_SENSITIVITY),
               () -> "Handle switch released"));
     }
   }
 
   @Override
-  public void exit() {}
+  public void exit() {
+    for (int cc = 0; cc < NUM_ENCODERS; cc++) {
+      sendMidiCc(RGB_ANIMATION_OUTPUT_CHANNEL, cc, RGB_MAX_BRIGHTNESS_VALUE);
+    }
+  }
 
   @Override
   public void flush() {
