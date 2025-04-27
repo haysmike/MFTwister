2cad4840a76d210e4d424ce1e25cea148cef5a34
Mike Hays <haysmikethere@gmail.com>
2025-04-26 22:33:35 -0700

    Turn off lights for inactive encoders, use modulated value

--- src/main/java/com/haysmike/MFTwisterExtension.java
+++ src/main/java/com/haysmike/MFTwisterExtension.java
@@ -1,13 +1,17 @@
 package com.haysmike;
 
+import com.bitwig.extension.api.util.midi.ShortMidiMessage;
 import com.bitwig.extension.controller.ControllerExtension;
 import com.bitwig.extension.controller.api.*;
 
 public class MFTwisterExtension extends ControllerExtension {
-    private static final int maxMidiCcValue = 127;
     private static final int numControls = 64;
+    private static final int maxMidiCcValue = 127;
+    public static final double defaultSensitivity = 1.0;
+    public static final double shiftedSensitivity = 0.5;
 
     private HardwareSurface hardwareSurface;
+    private MidiOut midiOut;
 
     protected MFTwisterExtension(final MFTwisterExtensionDefinition definition, final ControllerHost host) {
         super(definition, host);
@@ -18,24 +22,25 @@ public class MFTwisterExtension extends ControllerExtension {
         final ControllerHost host = getHost();
 
         MidiIn midiIn = host.getMidiInPort(0);
-        MidiOut midiOut = host.getMidiOutPort(0);
+        midiOut = host.getMidiOutPort(0);
 
         hardwareSurface = host.createHardwareSurface();
 
         for (int i = 0; i < numControls; i++) {
             final int cc = i;
-            RelativeHardwareKnob relativeHardwareKnob = hardwareSurface.createRelativeHardwareKnob("Knob " + cc);
+            final int encoderNumber = i + 1;
+            RelativeHardwareKnob relativeHardwareKnob = hardwareSurface.createRelativeHardwareKnob("Encoder " + encoderNumber);
             relativeHardwareKnob.setAdjustValueMatcher(midiIn.createRelativeBinOffsetCCValueMatcher(0, cc, maxMidiCcValue));
-            // TODO: Look into `HardwareLight`
-            relativeHardwareKnob.targetValue().addValueObserver(value -> midiOut.sendMidi(176, cc, (int) (value * maxMidiCcValue)));
+            relativeHardwareKnob.modulatedTargetValue().addValueObserver(value -> sendMidiCc(0, cc, (int) (value * maxMidiCcValue)));
+            relativeHardwareKnob.hasTargetValue().addValueObserver(value -> sendMidiCc(2, cc, value ? 47 : 17));
 
-            HardwareButton hardwareButton = hardwareSurface.createHardwareButton("Switch " + cc);
+            HardwareButton hardwareButton = hardwareSurface.createHardwareButton("Switch " + encoderNumber);
             HardwareAction pressedAction = hardwareButton.pressedAction();
             pressedAction.setActionMatcher(midiIn.createCCActionMatcher(1, cc, maxMidiCcValue));
-            pressedAction.setBinding(host.createCallbackAction(() -> relativeHardwareKnob.setSensitivity(0.5), () -> "Handle switch pressed"));
+            pressedAction.setBinding(host.createCallbackAction(() -> relativeHardwareKnob.setSensitivity(shiftedSensitivity), () -> "Handle switch pressed"));
             HardwareAction releasedAction = hardwareButton.releasedAction();
             releasedAction.setActionMatcher(midiIn.createCCActionMatcher(1, cc, 0));
-            releasedAction.setBinding(host.createCallbackAction(() -> relativeHardwareKnob.setSensitivity(1.0), () -> "Handle switch released"));
+            releasedAction.setBinding(host.createCallbackAction(() -> relativeHardwareKnob.setSensitivity(defaultSensitivity), () -> "Handle switch released"));
         }
     }
 
@@ -46,5 +51,9 @@ public class MFTwisterExtension extends ControllerExtension {
     @Override
     public void flush() {
         hardwareSurface.updateHardware();
+    }
+
+    private void sendMidiCc(int channel, int cc, int value) {
+        midiOut.sendMidi(ShortMidiMessage.CONTROL_CHANGE | channel, cc, value);
     }
 }
